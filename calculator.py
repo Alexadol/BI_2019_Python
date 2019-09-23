41c329b55b21cda4d5c570be8c2807da7b190d0d
Alexa <sqshadol@gmail.com>
2019-09-23 20:18:44 +0300

    Modify calcultor

--- calculator.py
+++ calculator.py
@@ -1,20 +1,43 @@
-a=int(input())
-c=str(input())
-b=int(input())
-if c in ['%','/','//'] and b==0:
-    print('Деление на 0!')
-elif c=='+':
-    print(a+b)
-elif c=='-':
-    print(a-b)
-elif c=='/':
-    print(a/b)
-elif c=='*':
-    print(a*b)
-elif c=='%':
-    print(a%b)
-elif c=='**':
-    print(a**b)
-elif c=='//':
-    print(a//b)
-
+while True:
+    while True:
+        first_number = input('Please enter first number: ')
+        if first_number.isdigit():
+            break
+        else:
+            print('Please, use digits only! Let\'s try again!')
+    operator = input('Please enter one of operator from [+,-,*,/]!/n Enter \'A\' for advanced version : ')
+    if operator == 'A':
+        print(
+            'To get integer remainder after dividing use \'//\'\nTo raise to a power use \'**\'\nTo get remainder from division use \'%\'')
+        operator = input('Please enter operator:')
+    while True:
+        second_number = input('Please enter first number: ')
+        if first_number.isdigit():
+            break
+        else:
+            print('Please, use digits only! Let\'s try again!')
+    a = first_number
+    b = second_number
+    c = operator
+    answer = 0
+    if c in ['%', '/', '//'] and second_number == 0:
+        print('If you like forbidden things division by zero is not the best choice:) Let\'s try again!')
+        second_number = input('Please enter second number! Use digits only :')
+    elif c == '+':
+        answer = (int(a) + int(b))
+    elif c == '-':
+        answer = (int(a) - int(b))
+    elif c == '/':
+        answer = (int(a) / int(b))
+    elif c == '*':
+        answer = (int(a) * int(b))
+    elif c == '%':
+        answer = (int(a) % int(b))
+    elif c == '**':
+        answer = (int(a) ** int(b))
+    elif c == '//':
+        answer = (int(a) // int(b))
+    print('Answer for your expression {}{}{} is {}'.format(first_number, operator, second_number, answer))
+    again = input('For new calculation enter \'new\', for exit enter \'exit\'')
+    if again == 'exit':
+        break
