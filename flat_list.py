9143e45e50e0d928f199f4b2c1a38cf83c9fd9e3
Alexadol <sqshadol@gmail.com>
2019-10-01 22:54:25 +0200

    Checkio corrections

--- flat_list.py
+++ flat_list.py
@@ -1,9 +1,8 @@
-
 def flat_list(some_list):
     string_list = str(some_list)
     new_string_for_list = ''
     for char in string_list:
-        if char not in ['[',']']:
+        if char not in ['[', ']']:
             new_string_for_list += char
     new_list = [int(el) for el in new_string_for_list.split(', ')]
     return new_list
